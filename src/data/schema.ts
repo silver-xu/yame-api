28a53821edf107c4ba6e0f4bc545a5e4a14f0a7e
Silver Xu <sxu@seek.com.au>
2019-03-19 11:02:54 +1100

    Fixing build

--- src/data/schema.ts
+++ src/data/schema.ts
@@ -9,7 +9,7 @@ type Query {
   oneOffKey: String
   currentUser: User
   defaultDoc: DefaultDoc
-  docAccesses: DocAccess[]
+  docAccesses: [DocAccess]
   doc(docId:String): Doc
   docByPermalink(username:String, permalink: String): Doc
 }
