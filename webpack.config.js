7a62d61a95c47903a3eb2bcaa61673510ec09981
Silver Xu <me@silverxu.com>
2019-04-18 15:55:04 +1000

    Fixing build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+var nodeModules = {
+    'html-pdf':'commonjs html-pdf'
+};
+
 module.exports = {
     entry: ['./dist/handler.js'],
     target: 'node',
+    externals: nodeModules,
     output: {
         path: `${process.cwd()}/pack`,
         filename: 'handler.js',
